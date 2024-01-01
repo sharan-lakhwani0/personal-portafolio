91eb4ee03696f142b708dbb9e6e799097bfc3d08
Sharan <slt00@iesemilidarder.com>
2024-01-01 16:03:48 +0100

    v2.5
    Almost done.

--- personal-portfolio/src/Header.js
+++ personal-portfolio/src/Header.js
@@ -3,20 +3,18 @@ import './Header.css';
 const Header = () => {
     return (
         <div id="header">
-        <div class="container">
+        <div className="container">
             <nav>
-                <h1 class="logo">Sharan</h1>
+                <h1 className="logo">Sharan</h1>
                 <ul id="sidemenu">
                     <li><a href="#header">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#services">Services</a></li>
                     <li><a href="#portfolio">Portfolio</a></li>
                     <li><a href="#contact">Contact</a></li>
-                    <i class="fa-solid fa-circle-xmark" onclick="closemenu()"></i>
                 </ul>
-                <i class="fa-solid fa-bars" onclick="openmenu()"></i>
             </nav>
-            <div class="header-text">
+            <div className="header-text">
                 <p>Web Developer specialized in REACT JS</p>
                 <h1>Hi I'm <span>Sharan L.</span> <br/> from Spain</h1>
 
